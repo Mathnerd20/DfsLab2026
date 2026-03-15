01b5b11a3919bbfa3006bcfa727dade8bd61c582
Mathnerd20 <25mcce28@uohyd.ac.in>
2026-03-16 00:09:48 +0530

    added indentation

--- lab3/Akshay/radix.c
+++ lab3/Akshay/radix.c
@@ -11,49 +11,49 @@ int main()
     int max = 0;
     for(int i = 0; i < n; i++)
     {
-	scanf("%d", &a[i]);
-	if(a[i] > max)
-	{
-	    max = a[i];
-	}
+		scanf("%d", &a[i]);
+		if(a[i] > max)
+		{
+	    	max = a[i];
+		}
     }
     int tab[n][10];
     int ten = 10;
     for(int i = 1; i <= dig(max); i++)
     {
-	for(int i = 0; i < n; i++)
-	{
-	     for(int j = 0; j < 10; j++)
-	     {
-		 tab[i][j] = 0; 
-	     }
-	}
-	for(int j = 0; j < n; j++)
-	{
-	    int k = 0;
-	    int num = (a[j] % ten)/(ten/10);
-	    while(tab[k][num] != 0)
-	    {
-		k++;
-	    }
-	    tab[k][num] = a[j];
-	}
-	int k = 0;
-	for(int j = 0; j < 10; j++)
-	{
-	    int row = 0;
-	    while(tab[row][j] != 0 && k < n)
-	    {
-		a[k] = tab[row][j];
-		row++;
-		k++;
-	    }
-	}
-	ten = ten * 10;
+		for(int i = 0; i < n; i++)
+		{
+	     	for(int j = 0; j < 10; j++)
+	     	{
+		 		tab[i][j] = 0; 
+	     	}
+		}
+		for(int j = 0; j < n; j++)
+		{
+		    int k = 0;
+		    int num = (a[j] % ten)/(ten/10);
+		    while(tab[k][num] != 0)
+		    {
+				k++;
+		    }
+		    tab[k][num] = a[j];
+		}
+		int k = 0;
+		for(int j = 0; j < 10; j++)
+		{
+		    int row = 0;
+		    while(tab[row][j] != 0 && k < n)
+		    {
+				a[k] = tab[row][j];
+				row++;
+				k++;
+		    }
+		}
+		ten = ten * 10;
     }
     for(int i = 0; i < n; i++)
     {
-	printf("%d ", a[i]); 
+		printf("%d ", a[i]); 
     }
 }
 
@@ -62,8 +62,8 @@ int dig(int a)
     int d = 0;
     while(a > 0)
     {
-	a = a/10;
-	d++;
+		a = a/10;
+		d++;
     }
     return d;
 }
